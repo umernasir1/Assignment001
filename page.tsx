18809a76b820d02debbee29a52e24e8af8ced1e3
umernasir1 <135062786+umernasir1@users.noreply.github.com>
2024-10-25 00:25:47 +0500

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,26 +1,18 @@
-import Link from 'next/link';
-import NavBar from '../components/NavBar';
+import NavBar from '../../components/NavBar';
 
-const countries = [
-  { name: 'Japan', slug: 'japan' },
-  { name: 'Brazil', slug: 'brazil' },
-  { name: 'Germany', slug: 'germany' },
-  { name: 'Australia', slug: 'australia' },
-  { name: 'Canada', slug: 'canada' },
-];
+export default function GermanyPage() {
+  const country = {
+    name: 'Germany',
+    population: '83,132,799',
+    capital: 'Berlin',
+  };
 
-export default function CountriesPage() {
   return (
     <div style={{ padding: '20px' }}>
       <NavBar />
-      <h1 style={{ color: 'navy' }}>Country List</h1>
-      <ol style={{ lineHeight: '2' }}>
-        {countries.map((country) => (
-          <li key={country.slug} style={{ marginBottom: '10px' }}>
-            <Link href={`/country/${country.slug}`}>{country.name}</Link>
-          </li>
-        ))}
-      </ol>
+      <h1 style={{ color: 'navy' }}>{country.name}</h1>
+      <p><strong>Population:</strong> {country.population}</p>
+      <p><strong>Capital:</strong> {country.capital}</p>
     </div>
   );
 }
